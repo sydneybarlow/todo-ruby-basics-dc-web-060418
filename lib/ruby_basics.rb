0b283eafede749877ac77eb64ffd383462b4b6a7
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-22 23:58:13 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+ return num1/num2
 end
 
 def assign_variable(value)
-  
+  puts value
+  "Bob"
 end
 
-def argue
-  
+def argue(nothing)
+  return nothing
 end
 
-def greeting
-
+def greeting(greeting, name)
+ 
 end
 
-def return_a_value
-
+def return_a_value(phrase)
+  new_phrase = "Nice"
+  return new_phrase
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase)
+  return phrase
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(food = "cheese")
+  return food
 end
